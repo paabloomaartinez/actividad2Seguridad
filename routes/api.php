7b144a0489a43b0cb3867875ff09ae4494ffc92f
raauulsiilvaa <raulsilvaalonso07@gmail.com>
2025-03-11 22:38:38 +0100

    Error de api.php solucionado

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route:
-post('/login', [AuthController::class, "login"]);
+Route::post('/login', [AuthController::class, "login"]);
