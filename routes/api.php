5267363b405afd3f462f4b8d480bea14f95f2892
paabloomaartinez <pablo121002@gmail.com>
2025-03-11 22:30:58 +0100

    Importando authController en api.php

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\AuthController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route:
-post('/login', [AuthController::class, "login"]);
+Route::post('/login', [AuthController::class, "login"]);
